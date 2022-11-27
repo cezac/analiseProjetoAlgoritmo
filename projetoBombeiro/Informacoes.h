98db3b8f5c6122c0fd13850628cfb7a00b836234
Caio Cezar Oliveira Filardi do Carmo <37853343+cezac@users.noreply.github.com>
2022-11-26 21:01:16 -0300

    Add files via upload

---
+++ projetoBombeiro/Informacoes.h
@@ -0,0 +1,17 @@
+//
+// Created by Caio- on 26/11/2022.
+//
+
+#ifndef MAIN_C_INFORMACOES_H
+#define MAIN_C_INFORMACOES_H
+
+typedef struct
+{
+    int i;
+    int f;
+    int tempo;
+}Rua;
+
+
+
+#endif //MAIN_C_INFORMACOES_H
